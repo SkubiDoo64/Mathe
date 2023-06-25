d2a2488dc70553aedab49c67e5d5bab8201fba11
Hubertus Sattler <Hubertus.sattler@outlook.de>
2023-06-25 09:42:27 +0200

    initialer comit

---
+++ Abstandsrechner/src/Punkt.java
@@ -0,0 +1,4 @@
+
+public class Punkt {
+public List<int> 
+}
---
+++ Abstandsrechner/src/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Punkt Gerade:");
+		
+	}
+
+}
