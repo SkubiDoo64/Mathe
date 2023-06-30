f62162a9d6484be44d7489a2157337a476f629c8
unknown <der.bunse@gmx.de>
2023-06-30 18:34:05 +0200

    mein zwischenstand

--- Abstandsrechner/src/main.java
+++ Abstandsrechner/src/main.java
@@ -5,13 +5,10 @@ public class main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("Punkt:");
-		Punkt point = new Punkt();
-		point.ConvertStringToPunkt("(6,10,2)");
+		Gerade gerade = new Gerade();
+		gerade.ConvertStringToGerade("(1,1,1)+s(3,2,5)");;
 		
-		Ebene ebene = new Ebene();
-		ebene.ConvertStringToEbene("(3,1,2)+s(1,1,6)+r(1,0,2)");
-		
-		System.out.println(Rechner.BerechneAbstand(point, ebene));
+		System.out.println(gerade.RichtungsVectorParameter + "\n");
+		System.out.println(gerade.StuetsVectorParameter + "\n");
 	}
 }
